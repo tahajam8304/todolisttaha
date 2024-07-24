db3d5cdb3ab34a44325d903c406d4f6e1010c5c5
taha <jamtaha170@gmail.com>
2024-07-24 18:38:06 +0800

    my changes

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,21 @@
 <template>
   <div>
-    <h1>Todo List</h1>
+    <h1 class="ww">Todo List</h1>
     <form @submit.prevent="addTodo">
-      <input v-model="newTodo" placeholder="Enter a task">
+      <input v-model="newTodo" placeholder="Enter a task" class="container">
       <button>Add</button>
     </form>
-    <ul>
-      <li v-for="(todo, index) in todos" :key="index">
-        {{ todo.text }}
-        <button @click="deleteTodo(index)">Delete</button>
-      </li>
-    </ul>
+      <ul class="border">
+         <li v-for="(todo, index) in todos" :key="index">
+          <label class="containers">
+                <input type="checkbox">
+               <span class="checkmark"></span>
+          </label>
+          {{ todo.text }}
+        <button @click="deleteTodo(index)">DELE</button>
+        </li>
+      </ul>
+    
   </div>
 </template>
 
@@ -39,5 +44,66 @@ export default {
 </script>
 
 <style>
-/* Add your custom styles here */
+
+body{
+  background-color: black;
+
+}
+
+ul{
+  width: 100px;
+  height: 100px;
+  /* flex: content; */
+  align-content: center;
+  align-items: center;
+  list-style-type: none;
+  margin-left: 700px;
+  padding-left: 45%;
+  padding-top: 50px;
+  /* display: ; */
+  /* text-decoration: none;
+  padding: 5px; */
+  margin: 5px;
+  display: inline;
+}
+li{
+  
+  /* border-bottom: 1px white solid; */
+  /* border-radius: 5px; */
+  color: white;
+  text-decoration: none;
+  padding-top: 10px;
+  
+}
+.ww{
+  color: red;
+}
+
+button{
+  background-color: blue;
+  color: white;
+  border: 2px solid black;
+  border-radius: 5px;
+  box-shadow: 5px;
+  width: 50px;
+  height: 35px;
+
+}
+button:hover{
+  background-color: red;
+  transition: 0.7s;
+}
+
+.container{
+  height: 30px;
+  width: 30%;
+  border: blue solid 2px;
+  border-radius: 5px;
+  color: red;
+  
+}
+
+
+
+
 </style>
